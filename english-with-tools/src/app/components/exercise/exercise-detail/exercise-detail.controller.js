efd264441740fb728f40e702a9b605f8642acbfa
denis-rodionov <den.iu7@gmail.com>
2019-01-24 02:56:23 +0100

    markup of fill-gaps exercise done

--- english-with-tools/src/app/components/exercise/exercise-detail/exercise-detail.controller.js
+++ english-with-tools/src/app/components/exercise/exercise-detail/exercise-detail.controller.js
@@ -42,6 +42,57 @@ function ExerciseDetailController() {
     const index = ctrl.exercise.sentences.indexOf(event.sentence);
     ctrl.exercise.sentences.splice(index, 1);
   };
+
+  ctrl.markup = function () {
+    var preExercise = "<div class=\"exercise\"><table>";
+    var postExercise = "</table><div class=\"controls\"><a class=\"button\" id=\"checkResult\" href=\"#\" draggable=\"false\">Check</a><a class=\"button\" id=\"resetResults\" href=\"#\" draggable=\"false\" style=\"visibility: hidden\">Reset</a></div><div id=\"result\"></div></div>";
+    var preText = "<tr><td><div class=\"text\"><p>";
+    var middleMarkup = "</p></div><div class=\"words\">";
+    var postText = "</div></td></tr>";
+    var preWord = "<span class=\"gap\" data-answer=\"";
+    var postWord = "\"></span>";
+    var separator = "#";
+
+    var result = "";
+    var wordsCounter = 0;
+    ctrl.exercise.sentences.forEach(function(sentence) {
+
+      // find words
+      var words = [];
+      sentence.words.split(',').forEach(function(word) {
+        words.push(word.trim());
+      });
+
+      var processedText = sentence.text;
+      sentence.text.split(separator).forEach(function(str, index) {
+        if (index % 2 == 1) {
+          words.push(str);
+          processedText = processedText.replace(separator + str + separator, preWord + str + postWord);
+        }
+      });
+      words = Shuffle(words);
+
+      // markup
+      var wordsMarkup = words.map(function(word) {
+        return "<div id=\"" + wordsCounter++ + "\" class=\"word\">" + word + "</div>";
+      }).join("");
+
+      result += preText + processedText + middleMarkup + wordsMarkup + postText;
+    });
+
+    ctrl.markupText = preExercise + result + postExercise;
+  }
+
+  ctrl.copyToClipboard = function() {
+    var textarea = document.getElementById("markup");
+    textarea.select();
+    document.execCommand("copy");
+  }
+
+  function Shuffle(o) {
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  };
 }
 
 angular
