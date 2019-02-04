1158759d5ef8b40ff679b1356ad9adc067345a91
denis-rodionov <den.iu7@gmail.com>
2019-02-04 21:54:43 +0100

    new exercise template: Dropped Gaps

---
+++ exercise-templates/dropped-gaps/dropped-gaps.js
@@ -0,0 +1,50 @@
+document.getElementById("ew-check-results").onclick = function() {
+	var gaps = document.getElementsByClassName('ew-gap-option');
+  
+  var correctAnswers = 0;
+  for (var i = 0; i < gaps.length; i++) {
+  	if (gaps[i].value === gaps[i].dataset.correct) {
+    	correctAnswers++;
+      gaps[i].classList.add('right');
+    } else {
+    	gaps[i].classList.add('wrong');
+    }
+  }
+  
+  showResult(correctAnswers, gaps.length);
+}
+
+// --------  Common functions -------------------
+
+function showResult(score, total) {
+  var resultText = "Набрано баллов: " + score + " из " + total;
+  document.getElementById("ew-result-text").innerText = resultText;
+  sendAnswer(resultText);
+}
+
+function sendAnswer(answer) {
+  var answerInput = document.getElementById('lessonanswer-answer_text');
+
+  if (answerInput) {
+    console.log('answer input found!')
+    answerInput.innerText = answer;
+    var sendButton = document.getElementsByName('send-answer')[0];
+    sendButton.click();
+  } else {
+    //var answerPanel = document.getElementsByClassName('answer-')
+    var commentIntputs = document.getElementsByClassName('new-comment-textarea');
+    if (commentIntputs.length == 0) {
+      console.log('comment input not found!')
+    } else {
+      console.log('comment input found. inserting answer: ' + answer);
+      commentIntputs[0].innerText = answer;
+      var sendButtons = document.getElementsByClassName('btn-send');
+      if (sendButtons.legnth == 0) {
+        console.log('not found a button to send a comment');
+      } else {
+        console.log('send comment button found');
+        sendButtons[0].click();
+      }
+    }
+  }
+}
