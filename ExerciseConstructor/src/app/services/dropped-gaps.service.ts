c7ac56139f7f25bb4c78bc06fabaeb5eee5813f6
denis-rodionov <den.iu7@gmail.com>
2019-02-07 08:08:59 +0100

    remove empty option for DroppedGaps exercise type

--- ExerciseConstructor/src/app/services/dropped-gaps.service.ts
+++ ExerciseConstructor/src/app/services/dropped-gaps.service.ts
@@ -19,10 +19,10 @@ export class DroppedGapsService {
         let processedSentence = sentence.text;
         sentence.text.split(separator).forEach(function (str, index) {
             if (index % 2 === 1) {
-              const words = str.split(',').map(s => s.trim());
+              const words = str.split(',').map(s => s.trim()).filter(s => s !== '');
               const wordsMarkup = _this.commonService.shuffle(words.map(s => '<option value="' + s + '">' + s + '</option>')).join('');
 
-              const preGap = '<select class="ew-gap-option" data-correct="' + words[0] + '"><option></option>';
+              const preGap = '<select class="ew-gap-option" data-correct="' + words[0] + '">';
 
               processedSentence = processedSentence.replace(separator + str + separator, preGap + wordsMarkup + '</select>');
             }
